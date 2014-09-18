8a05dd031c441d2bf6bf26178c7e29ec1c8b9d4f
Manuel Gonzales <macleod2486@gmail.com>
2014-09-18 14:15:13 -0500

    Started redoing client

--- src/client.py
+++ src/client.py
@@ -23,6 +23,7 @@ from getpass import getpass
 from keys import KeyManager
 from server import Server
 
+import platform
 import socket
 import os.path
 import os
@@ -65,79 +66,35 @@ def Commands (arguments):
 	if command[0] == "help":
 		print("\nCommands available\nconnect ip hostname portNo\ndisconnect\nkeygen password password\nunlock password\nclear\nstartserver\nstopserver")
 
-	#Attempts to connect to the server and obtain the username for their public key to be used
-	elif (command[0] == "connect") and unlocked:
-		try:
-			global recipantUser, clientSocket, servMd5, clienthost
-			clienthost = command[1]
-			port = command[2]
-			clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			clientSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		
-			clientSocket.connect((clienthost,int(port)))
-			recipantUser = clientSocket.recv(1024)
-			
-			#Sends the server your username
-			clientSocket.send(username)
-
-			#Recieves the checksum of the public key the server has
-			servMd5 = clientSocket.recv(1024)
-			
-			#Checks to see if the key is currently stored		
-			md5 = KeyCheckSum()
-			if md5.CurrentAuthorized(clienthost,servMd5):
-				print "Recognized host"
-			else:
-				authorizedHost = AuthorizedHosts(root)
-				root.wait_window(authorizedHost.top)
-
-			connected = True
-		except IndexError:
-			connected = False
-		except Exception as e:
-			print("Error in connecting")
-			connected = False
-			print(e)
-
-		#Then checks to see if the users public key exists
-                try:
-			if connected:
-		                #Checks for public key
-	                        global publicKey
-	                        checkPuFile = open(os.path.dirname(__file__)+'/../keys/'+recipantUser+'pub.key','r')
-	                        publicKey = RSA.importKey(checkPuFile.read())
-	                        checkPuFile.close()
-                except Exception as e:
-                        print("Error in obtaining users public key")
-                        print(e)
-
-	#Will disconnect from server when prompted		
-	elif command[0] == 'disconnect':
-		try:
-			clientSocket.shutdown(2)
-			clientSocket.close()
-			print("Disconnected")
-		except:
-			print("No connection")
-
 	#Generates the keys with the password specified
 	elif command[0] == 'keygen':
 		try:
 			keymanage.keyCreate()
-			
 		except IndexError:
 			print("Error in creating keys")
 
 	#Unlock keys
 	elif command[0] == 'unlock':
-		keymanage.keyUnlock()
+		unlocked = keymanage.keyUnlock()
+		if unlocked:
+			privateKey = keymanage.getKey()
 
 	elif command[0] == 'clear':
-		os.system('clear')
+		if platform.system() == 'Linux':
+			os.system('clear')
+
+		elif platform.system() == 'OSX':
+			os.system('clear')
+
+		elif platform.system() == 'Windows':
+			os.system('cls')
+
 	elif command[0] == 'startserver':
 		startServer()
+
 	elif command[0] == 'stopserver':
 		stopServer()
+
 	else:
 		print("Command not found")
 		print("\nCommands available\nconnect ip hostname portNo\ndisconnect\nkeygen password password\nunlock password\nclear\nstartserver\nstopserver")
@@ -150,11 +107,15 @@ def startServer():
 #Stops both the server and client
 def stopServer():
 	serverInstance.stopServer()
-	print ("Server stopped")
 
 #Takes in user commands and messages
 def sendMessage():
 	print("Message sent")
+
+#--------------------------------------------------------------------------
+
+#Main running of the code
+
 
 #Obtains the necessary info from config files
 try:
@@ -181,10 +142,6 @@ except Exception as e:
         print("Error in config file!")
 	print(e)
         exit(1)
-
-#--------------------------------------------------------------------------
-
-#Main running of the code
 
 print ("\n******************************")
 print ("Welcome to the Whisper program")
